a108ef9ab81621e4fcb0bb53b3cc29f673c66955
Deag11e <sup2.bws@gmail.com>
2022-10-11 18:49:40 +0300

    PEP8

---
+++ 8_Lesson.py
@@ -0,0 +1,27 @@
+# OПП Урок 8
+# разбор домашки 1
+
+# some_list = [2, 3, 4, 'gg', 'dfg', 4, True,'False']
+# some_list = list (filter(lambda x: isinstance(x, str)))
+# print(some_list)
+
+# разбор домашки 2
+# users = {
+#     1: { 'name': 'vasya',
+#          'email': 'info@info.com'
+#     },
+#     2: { 'name': 'masha',
+#          'email': 'info@info.com'
+#     }
+#     3: { 'name': 'masha',
+#          'email': 'info@info.com'
+#     }
+# }
+# разбор домашки 3 (четные не четные сортировка)
+# from _random import randint
+# numbers = [randint(1,10) for _ in range(10)]
+# i = 0
+# while i < len(numbers)
+#     if not nambers[i]% 2 :
+#         i+=1
+# print(numbers)
